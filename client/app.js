33484e0f0a1a4ada408d95872ee63edd70fd85c2
Ryan Bates <bates812@hotmail.com>
2017-03-30 18:36:16 -0500

    Added style

--- client/app.js
+++ client/app.js
@@ -1,18 +1,19 @@
 var tree = require('./tree');
 
 $.get('/trees', function(trees){
-  $('body').html(tree.list(trees));
+  $('body').addClass("jumbotron").html(tree.list(trees));
 });
 
 $.get('/trees', function(trees, status){
   if(status == "success"){
-    $('<button>').text('Add Tree').on('click', function(){
+    $('<button>').text('Add Tree').addClass("btn-lg center-block").on('click', function(){
       //Add form to the page
       $('body').load('/public/project-form.html', function(){
         //Overide default form action
         $('form').on('submit', function(event){
           event.preventDefault();
           var data = new FormData($('form')[0]);
+          console.log(data);
           $.post({
             url: '/trees',
             data: data,
--- public/app.js
+++ public/app.js
@@ -2,18 +2,19 @@
 var tree = require('./tree');
 
 $.get('/trees', function(trees){
-  $('body').html(tree.list(trees));
+  $('body').addClass("jumbotron").html(tree.list(trees));
 });
 
 $.get('/trees', function(trees, status){
   if(status == "success"){
-    $('<button>').text('Add Tree').on('click', function(){
+    $('<button>').text('Add Tree').addClass("btn-lg center-block").on('click', function(){
       //Add form to the page
       $('body').load('/public/project-form.html', function(){
         //Overide default form action
         $('form').on('submit', function(event){
           event.preventDefault();
           var data = new FormData($('form')[0]);
+          console.log(data);
           $.post({
             url: '/trees',
             data: data,
@@ -32,36 +33,18 @@ module.exports = {
 };
 
 function list(trees){
-  upload();
-  var table = $('<table>').addClass('table');
-  var head = $('<tr>').append('<th>Name</th>', '<th>Description</th>', '<th>Image</th>').appendTo(table);
+  var table = $('<table>').addClass('table').attr('align', 'center');
+  var head = $('<tr>').append('<th class="text-center">Tree Gallery</th>').appendTo(table);
 
   trees.forEach(function(tree){
-    var row = $('<tr>').append(
-      $('<td>').text(tree.name),
-      $('<td>').text(tree.description),
+    var imgRow = $('<tr>').append(
       $('<td>').append('<a href="/trees/' + tree.id + '"><img src="' + tree.image + '"/></a>')
+    ).appendTo(table);
+    var nameRow = $('<tr>').append(
+      $('<td>').text(tree.name)
     ).appendTo(table);
   });
   return table;
-}
-function upload(){
-  $('<button>').text('Add Tree').on('click', function(){
-    //Add form to the page
-    $('body').load('/public/project-form.html', function(){
-      //Overide default form action
-      $('form').on('submit', function(event){
-        event.preventDefault();
-        var data = new FormData($('form')[0]);
-        $.post({
-          url: '/trees',
-          data: data,
-          contentType: 'multipart/form-data',
-          processData: false
-        });
-      });
-    });
-  }).appendTo('body');
 }
 
 },{}]},{},[1]);
